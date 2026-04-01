f5855e5193c13dc8ea27487057d49a65b1cd68f9
Shriyans <shriyans.singh@stonybrook.edu>
2026-04-01 01:59:27 -0400

    idk

--- src/hw6.c
+++ src/hw6.c
@@ -1,5 +1,10 @@
 #include "hw6.h"
+//Shriyans Singh 114807762
 
 int main(int argc, char *argv[]) {
+    //if not enough arguments provided return missing argument from header file
+    if(argc<7){
+        return MISSING_ARGUMENT;
+    }
     return 0;
 }
