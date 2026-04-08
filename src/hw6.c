5e460917a0f9e983791d0eaec592ee12ae61558d
Shriyans <shriyans.singh@stonybrook.edu>
2026-04-08 00:57:37 -0400

    working on part 1

--- src/hw6.c
+++ src/hw6.c
@@ -6,8 +6,20 @@ int main(int argc, char *argv[]) {
     if(argc<7){
         return MISSING_ARGUMENT;
     }
-    
+    //Using Max_search_len from header file to store the search string+1 because it also needs to include the null terminator
+    char s_Search[MAX_SEARCH_LEN+1];
+    char r_Replace[MAX_SEARCH_LEN+1];
+    bool wildcard=false;
+    //EOF = -1, which would be an extremely large number, and startIndex starts at 1 according to bullet 4 of overview
+    int startIndex=1;
+    int endIndex=9999;
+    bool s_Exists=false;
+    bool r_Exists=false;
+    bool l_Exists=false;
+    bool w_Exists=false;
 
+    //Loops through arguments and checks if it should return DUPLICATED_ARGUMENTS
+    
 
 
 
